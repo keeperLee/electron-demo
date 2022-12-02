c8172b9d841d3d5551ad920b2f865403cfc8adc1
lijian <lijian@52tt.com>
2022-12-02 15:26:29 +0800

    使用url拦截-创建窗口

--- main.js
+++ main.js
@@ -1,14 +1,58 @@
-const { app, BrowserWindow } = require('electron')
+//1.引用本地文件，创建窗口
+// const { app, BrowserWindow } = require('electron')
+// const createWindow = () => {
+//     const win = new BrowserWindow({
+//         width: 800,
+//         height: 600,
+//     })
+//
+//     win.loadFile('index.html')
+// }
+//
+// app.whenReady().then(() => {
+//     createWindow()
+// })
+
+
+//2.加载链接，创建窗口
+// const {app,BrowserWindow} = require('electron');
+// const createWindow = () => {
+//     const win = new BrowserWindow({ width: 800, height: 1500 })
+//     win.loadURL('https://github.com')
+//     const contents = win.webContents
+//     console.log(contents)
+//     console.log(111)
+// }
+// app.whenReady().then(()=>{
+//     createWindow()
+// })
+
+
+
+//3.适配win和mac
+const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-    })
+    });
 
-    win.loadFile('index.html')
-}
+    win.loadFile('index.html');
+};
 
 app.whenReady().then(() => {
-    createWindow()
-})
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
