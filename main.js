5ef70bb3d59ff7c162cb414249a863ace2da423f
lijian <lijian@52tt.com>
2022-12-02 16:21:56 +0800

    使用ipcMain和ipc

--- main.js
+++ main.js
@@ -58,7 +58,27 @@
 // });
 
 
-const { app, BrowserWindow } = require('electron')
+// const { app, BrowserWindow } = require('electron')
+// const path = require('path')
+//
+// const createWindow = () => {
+//     const win = new BrowserWindow({
+//         width: 800,
+//         height: 600,
+//         webPreferences: {
+//             preload: path.join(__dirname, 'preload.js'),
+//         },
+//     })
+//
+//     win.loadFile('index.html')
+// }
+//
+// app.whenReady().then(() => {
+//     createWindow()
+// })
+
+
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
@@ -69,10 +89,7 @@ const createWindow = () => {
             preload: path.join(__dirname, 'preload.js'),
         },
     })
-
+    ipcMain.handle('ping', () => 'pong')
     win.loadFile('index.html')
 }
-
-app.whenReady().then(() => {
-    createWindow()
-})
+app.whenReady().then(createWindow)
--- renderer.js
+++ renderer.js
@@ -1,2 +1,12 @@
 const information = document.getElementById('info')
-information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+// information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+
+
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // 打印 'pong'
+    information.innerText = response
+}
+
+func()
