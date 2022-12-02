1d61704799291a2f906d9fcec92d4b9a7b2baab3
lijian <lijian@52tt.com>
2022-12-02 18:07:27 +0800

    渲染进程向主进程通信（单向）

--- preload.js
+++ preload.js
@@ -1,3 +1,4 @@
+//contextBridge
 // const { contextBridge } = require('electron')
 //
 // contextBridge.exposeInMainWorld('versions', {
@@ -8,13 +9,24 @@
 // })
 
 
+//渲染进程向主进程通信（双向）
+// const { contextBridge, ipcRenderer } = require('electron')
+//
+// contextBridge.exposeInMainWorld('versions', {
+//     node: () => process.versions.node,
+//     chrome: () => process.versions.chrome,
+//     electron: () => process.versions.electron,
+//     ping: () => ipcRenderer.invoke('ping'),
+//     // 能暴露的不仅仅是函数，我们还可以暴露变量
+// })
 
+
+//渲染进程向主进程通信（单向）
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-    ping: () => ipcRenderer.invoke('ping'),
-    // 能暴露的不仅仅是函数，我们还可以暴露变量
+contextBridge.exposeInMainWorld('electronAPI', {
+    setTitle: (title) => ipcRenderer.send('set-title', title)
 })
+
+
+
--- renderer.js
+++ renderer.js
@@ -1,12 +1,18 @@
-const information = document.getElementById('info')
+//contextBridge获取变量
+// const information = document.getElementById('info')
 // information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`
+// const func = async () => {
+//     const response = await window.versions.ping()
+//     console.log(response) // 打印 'pong'
+//     information.innerText = response
+// }
+// func()
 
+//渲染进程向主进程通信（单向）
+const setButton = document.getElementById('btn')
+const titleInput = document.getElementById('title')
+setButton.addEventListener('click', () => {
+    const title = titleInput.value
+    window.electronAPI.setTitle(title)
+});
 
-
-const func = async () => {
-    const response = await window.versions.ping()
-    console.log(response) // 打印 'pong'
-    information.innerText = response
-}
-
-func()
